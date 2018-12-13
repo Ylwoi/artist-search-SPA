6d01e2414a83d6485eb0b0ef82b63644512bd9c7
Ylwoi <balazs.horvath989@gmail.com>
2018-12-13 14:44:59 +0100

    Get artists data by user search from iTunes API

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,30 @@ interface ISearchStates {
 class App extends React.Component<{}, ISearchStates> {
   constructor (props: any) {
     super(props);
-    
+
     this.updateValue = this.updateValue.bind(this);
     this.searchArtist = this.searchArtist.bind(this);
   }
   public searchArtist () {
-    console.log(this.state.searchText);
+    const searchTerm: string = this.state.searchText.split(' ').join('+');
+    const searchURL: string = 'https://itunes.apple.com/search?term=' + searchTerm + '&entity=allArtist&attribute=allArtistTerm';
+
+    fetch(searchURL)
+      .then((response: any) => {
+        if(response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then((artists: any) => {
+        console.log(artists.results);
+      })
+      .catch((error: any) => {
+        console.error(error.massege);
+      })
   }
   public updateValue (evt: any) {
     this.setState({searchText: evt.target.value});
-    console.log(evt.target.value);
   }
   public render() {
     return (
